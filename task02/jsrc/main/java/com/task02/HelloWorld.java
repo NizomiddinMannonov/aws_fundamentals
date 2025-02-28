39fec5e1f583be00271c602344ba1806b1fac0c8
NizomiddinMannonov <nizom20001975@gmail.com>
2025-02-28 16:36:28 +0500

    ,,,,,

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,54 +1,38 @@
 package com.task02;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
-//import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-//import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+    roleName = "hello_world-role",
+    isPublishVersion = false,
+    aliasName = "${lambdas_alias_name}",
+    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
-)
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
-		context.getLogger().log(input.toString());
-		context.getLogger().log(context.toString());
-		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Content-Type", "application/json");
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+        String path = (String) event.getOrDefault("rawPath", "/");
+        String method = ((Map<String, String>) event.get("requestContext")).get("httpMethod");
 
-		String path = input.getRawPath();
-		String httpMethod = input.getRequestContext().getHttp().getMethod();
+        Map<String, Object> response = new HashMap<>();
+        response.put("headers", Map.of("Content-Type", "application/json"));
 
-		if ("/hello".equals(path)) {
-			response.setStatusCode(200);
-			response.setHeaders(headers);
-			response.setBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
-		} else {
-			response.setStatusCode(400);
-			response.setHeaders(headers);
-			response.setBody(String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, httpMethod));
-		}
+        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+            response.put("statusCode", 200);
+            response.put("message", "{\"message\": \"Hello from Lambda\"}");
+        } else {
+            String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method);
+            response.put("statusCode", 400);
+            response.put("message", String.format("{\"message\": \"%s\"}", errorMessage));
+        }
 
-		return response;
-	}
+        return response;
+    }
 }
