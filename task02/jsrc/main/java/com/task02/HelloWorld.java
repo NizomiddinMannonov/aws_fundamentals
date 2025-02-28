5455dc788b4a5fb9308a5cc6d706c49c430c57b2
NizomiddinMannonov <nizom20001975@gmail.com>
2025-02-28 18:25:49 +0500

    path

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -19,24 +19,28 @@ public class HelloWorld implements RequestHandler<Map<String, Object>, Map<Strin
 
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+        System.out.println("Received event: " + event);  // Debugging log to see what input is coming in
+
+        // Extract request details
         String path = (String) event.getOrDefault("rawPath", "/");
-        String method = ((Map<String, String>) event.get("requestContext")).get("httpMethod");
+        Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+        Map<String, String> http = (Map<String, String>) requestContext.get("http");
+        String method = http.get("method");
+
+        // Debugging logs
+        System.out.println("Extracted Path: " + path);
+        System.out.println("Extracted Method: " + method);
 
         Map<String, Object> response = new HashMap<>();
         response.put("headers", Map.of("Content-Type", "application/json"));
 
-        // Fix the response formatting
         if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-            Map<String, String> responseBody = new HashMap<>();
-            responseBody.put("message", "Hello from Lambda");
             response.put("statusCode", 200);
-            response.put("body", responseBody);
+            response.put("body", "{\"message\": \"Hello from Lambda\"}");
         } else {
             String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method);
-            Map<String, String> errorBody = new HashMap<>();
-            errorBody.put("message", errorMessage);
             response.put("statusCode", 400);
-            response.put("body", errorBody);
+            response.put("body", String.format("{\"message\": \"%s\"}", errorMessage));
         }
 
         return response;
