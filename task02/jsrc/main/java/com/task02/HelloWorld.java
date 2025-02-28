864b20a07f5883dec3f3faedf76b85d6299069f8
NizomiddinMannonov <nizom20001975@gmail.com>
2025-02-28 18:23:10 +0500

    ..

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,4 +1,5 @@
 package com.task02;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
@@ -10,7 +11,7 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "hello_world",
     roleName = "hello_world-role",
-    isPublishVersion = false,
+    isPublishVersion = true,
     aliasName = "${lambdas_alias_name}",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
@@ -24,13 +25,18 @@ public class HelloWorld implements RequestHandler<Map<String, Object>, Map<Strin
         Map<String, Object> response = new HashMap<>();
         response.put("headers", Map.of("Content-Type", "application/json"));
 
+        // Fix the response formatting
         if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+            Map<String, String> responseBody = new HashMap<>();
+            responseBody.put("message", "Hello from Lambda");
             response.put("statusCode", 200);
-            response.put("message", "{\"message\": \"Hello from Lambda\"}");
+            response.put("body", responseBody);
         } else {
             String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, method);
+            Map<String, String> errorBody = new HashMap<>();
+            errorBody.put("message", errorMessage);
             response.put("statusCode", 400);
-            response.put("message", String.format("{\"message\": \"%s\"}", errorMessage));
+            response.put("body", errorBody);
         }
 
         return response;
